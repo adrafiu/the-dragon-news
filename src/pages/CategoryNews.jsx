8e06809c7dc939b5c1713babd19c098e41864b54
adrafiu <rafiu.diu@gmail.com>
2025-10-19 02:03:24 +0600

    added routes,components,pages and  more section

---
+++ src/pages/CategoryNews.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CategoryNews = () => {
+    return (
+        <div>
+            <h1>CategoryNews</h1>
+            
+        </div>
+    );
+};
+
+export default CategoryNews;
